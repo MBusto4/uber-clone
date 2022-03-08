22e8e872c4176e53e033713763556952e1291c6e
MBusto <mikebusto4@gmail.com>
2022-03-08 16:47:44 -0500

    Project setup with nextjs and tailwindcss

---
+++ uber-blockchain-clone/pages/index.js
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const style = {
+  wrapper: `h-screen w-screen flex flex-col`,
+  main: `h-full w-screen flex-1 z-10`,
+  mapContainer: `flex-1 w-full h-full`,
+  rideRequestContainer: `h-full w-[400px] ml-[1rem] py-[3rem] absolute top-0 left-0 flex flex-col justify-end z-20`,
+  rideRequest: `h-full max-h-[700px] bg-white rounded-lg flex flex-col overflow-scroll`,
+}
+
+export default function Home() {
+  return (
+    <div className={style.wrapper}>
+      {/*NavBar*/}
+      <div className={style.main}>
+        {/*map*/}
+      </div>
+      <div className={style.rideRequestContainer}>
+        <div className={style.rideRequest}>
+          {/*Location selector*/}
+          {/* confirm ride */}
+        </div>
+      </div>
+    </div>
+  )
+}
